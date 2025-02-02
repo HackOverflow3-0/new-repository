0445939eb00afcf7f985b4b2dbcb7562ab52482f
VIVEK KOKAMBE <vivekkokambe612@gmail.com>
2025-02-03 05:07:12 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {Swiper,SwiperSlide} from "swiper/react";
 
 
@@ -10,7 +10,7 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import 'swiper/css/autoplay';
 
-import {EffectCoverflow,Pagination,Navigation  } from "swiper/modules";
+import {EffectCoverflow,Pagination,Navigation,Autoplay  } from "swiper/modules";
 
 import slide_image_1 from "./assets/images/mix1.jpeg";
 import slide_image_2 from "./assets/images/mix2.jpeg";
@@ -20,12 +20,30 @@ import slide_image_5 from "./assets/images/mix5.jpeg";
 import slide_image_6 from "./assets/images/mix6.jpeg";
  
 function App() {
+
+  const [swiperInstance, setSwiperInstance] = useState(null);
+
+  // Handle autoplay stop on hover
+  const handleMouseEnter = () => {
+    if (swiperInstance) swiperInstance.autoplay.stop();
+  };
+
+  // Handle autoplay resume when mouse leaves
+  const handleMouseLeave = () => {
+    if (swiperInstance) swiperInstance.autoplay.start();
+  };
+
   return (
     <>
-    <div className="mainSection">
+    <div className="mainSection"
+     onMouseEnter={handleMouseEnter} // Stop autoplay on hover
+     onMouseLeave={handleMouseLeave}
+    >
     <div className="container">
       <h1 className="heading">Glimpses of Hack Overflow 2.0</h1>
       <Swiper
+       
+        onSwiper={setSwiperInstance}
         effect={'coverflow'}
         grabCursor={true}
         centeredSlides={true}
@@ -37,15 +55,21 @@ function App() {
           depth: 100,
           modifier: 2.5,
         }}
-        
+         
+        autoplay={{
+          delay: 2000,  // Slides change every 3 seconds
+        }}
+        speed={800}
         pagination={{ el: '.swiper-pagination', clickable: true }}
         navigation={{
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev',
           clickable: true,
         }}
-        modules={[EffectCoverflow, Pagination, Navigation]}
+        modules={[EffectCoverflow, Pagination, Navigation,Autoplay]}
         className="swiper_container"
+        onMouseEnter={handleMouseEnter} // Stop autoplay on hover
+        onMouseLeave={handleMouseLeave}
       >
         <SwiperSlide>
           <img src={slide_image_1} alt="slide_image" />
@@ -84,195 +108,7 @@ function App() {
       </Swiper>
     </div>
     </div>
-    <div className="mainSection">
-    <div className="container">
-      <h1 className="heading">Glimpses of Hack Overflow 2.0</h1>
-      <Swiper
-        effect={'coverflow'}
-        grabCursor={true}
-        centeredSlides={true}
-        loop={true}
-        slidesPerView={'auto'}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 0,
-          depth: 100,
-          modifier: 2.5,
-        }}
-        
-        pagination={{ el: '.swiper-pagination', clickable: true }}
-        navigation={{
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-          clickable: true,
-        }}
-        modules={[EffectCoverflow, Pagination, Navigation]}
-        className="swiper_container"
-      >
-        <SwiperSlide>
-          <img src={slide_image_1} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_2} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_3} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_4} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_5} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_6} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_5} alt="slide_image" />
-        </SwiperSlide>
-        
-        
-        <div className="slider-controler">
-          <div className="swiper-button-prev slider-arrow">
-            <ion-icon name="arrow-back-outline"></ion-icon>
-          </div>
-          
-          <div className="swiper-button-next slider-arrow">
-            <ion-icon name="arrow-forward-outline"></ion-icon>
-          </div>
-          
-        </div>
-        <div className="swiper-pagination"></div>
-      </Swiper>
-    </div>
-    </div>
-    <div className="mainSection">
-    <div className="container">
-      <h1 className="heading">Glimpses of Hack Overflow 2.0</h1>
-      <Swiper
-        effect={'coverflow'}
-        grabCursor={true}
-        centeredSlides={true}
-        loop={true}
-        slidesPerView={'auto'}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 0,
-          depth: 100,
-          modifier: 2.5,
-        }}
-        
-        pagination={{ el: '.swiper-pagination', clickable: true }}
-        navigation={{
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-          clickable: true,
-        }}
-        modules={[EffectCoverflow, Pagination, Navigation]}
-        className="swiper_container"
-      >
-        <SwiperSlide>
-          <img src={slide_image_1} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_2} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_3} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_4} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_5} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_6} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_5} alt="slide_image" />
-        </SwiperSlide>
-        
-        
-        <div className="slider-controler">
-          <div className="swiper-button-prev slider-arrow">
-            <ion-icon name="arrow-back-outline"></ion-icon>
-          </div>
-          
-          <div className="swiper-button-next slider-arrow">
-            <ion-icon name="arrow-forward-outline"></ion-icon>
-          </div>
-          
-        </div>
-        <div className="swiper-pagination"></div>
-      </Swiper>
-    </div>
-    </div>
-    <div className="mainSection">
-    <div className="container">
-      <h1 className="heading">Glimpses of Hack Overflow 2.0</h1>
-      <Swiper
-        effect={'coverflow'}
-        grabCursor={true}
-        centeredSlides={true}
-        loop={true}
-        slidesPerView={'auto'}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 0,
-          depth: 100,
-          modifier: 2.5,
-        }}
-        
-        pagination={{ el: '.swiper-pagination', clickable: true }}
-        navigation={{
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-          clickable: true,
-        }}
-        modules={[EffectCoverflow, Pagination, Navigation]}
-        className="swiper_container"
-      >
-        <SwiperSlide>
-          <img src={slide_image_1} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_2} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_3} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_4} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_5} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_6} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_5} alt="slide_image" />
-        </SwiperSlide>
-        
-        
-        <div className="slider-controler">
-          <div className="swiper-button-prev slider-arrow">
-            <ion-icon name="arrow-back-outline"></ion-icon>
-          </div>
-          
-          <div className="swiper-button-next slider-arrow">
-            <ion-icon name="arrow-forward-outline"></ion-icon>
-          </div>
-          
-        </div>
-        <div className="swiper-pagination"></div>
-      </Swiper>
-    </div>
-    </div>
-    
      
-
     
     </>
     
